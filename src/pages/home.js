3db94b0cce65e496116c4f3557dd3ddfc36b416a
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-06-17 07:00:25 +0100

    make changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,235 +9,56 @@ const Home = () => {
     console.log(firstTwoItems);
     return (
         <div className='container-flex'>
-            <div className='main-container'>
-                <div className='container-card'>
-                    <div className='col1'>
-                        <h1><span>Featured</span> This month</h1>
-                    {firstTwoItems.map((item, key)=>{
-                        return(
-                            <a href='/'>
-                            <div key={key} className='main-section'>
-                                <div>
-                                <img src={item.image} alt='photo'  />    
-                                </div>
-                                <div className='text-sec'>
-                                <div>
-                                    <span className='category'>{item.category}</span>
-                                </div>
-                                <div>
-                                    <h1>{item.title}</h1>
-                                </div>
-                                <div className='info'>
-                                    <span>
-                                     <img className='author-pic' src={item.authorPicture} alt='photo' />   
-                                    </span>
-                                    <span>{item.authorName}</span>
-                                    <span>{item.date}</span>
-                                    <span>{item.time}</span>
-                                </div>
-                                <div>
-                                    <p>{item.desc}</p>
-                                </div>
-                                </div>
-                            </div>
-                            </a>
-                        )
-                    })}
-                    </div>
-                    <div className='divider'></div>
-                    <div className='col2'>
-                        <h3><span>popular</span> Posted</h3>
-                        <div className='wrapper1'>
-                        {firstfourItems.map((item, key)=>{
-                            return(
-                                <div className='main-section2'>
-                                <a href='/'>
-                                <div  key={key} className='main-flex'>
-                                <div>
-                                <img src={item.image}  alt='photo' />  
-                                </div>
-
-                                <div className='text-sec2'>
-                                <div>
-                                    <span className='category'>{item.category}</span>
-                                </div>
-                                <div>
-                                    <h5>{item.title}</h5>
-                                </div>
-                                <div className='info'>
-                                <span> <img className='author-pic'src={item.authorPicture} alt='photo' /></span>
-                                    <span>{item.authorName}</span>
-                                    <span>{item.time}. To Read</span>
-                                </div>  
-                                </div>
-                               </div>
-                               </a>
-                                </div>
-                            )
-                        })}
+            <div className='container-one'>
+                <div className='col1 mb-5'>
+                    <h3 className='mb-5'><span>Featured</span> This month</h3>
+               { firstTwoItems.map((item)=>{
+                    return(
+                        <div className='row-flex'>
+                           <div className='col'>
+                           <img src={item.image} alt='travel' className='image-one' />
+                           </div>
+                           <div className='card-text'>
+                           <span className='category'>{item.category}</span>
+                           <h1 className='title'>{item.title}</h1>
+                           <ul className='list-unstyled d-flex flex-row flex-wrap gap-3'>
+                            <li><img src={item.authorPicture} alt='authors phot'  className='iamge-round'/></li>
+                            <li className='text-nowrap'>{item.date}</li>
+                            <li className='text-nowrap'>{item.time}</li>
+                           </ul>
+                           <p className='w-76'>{item.desc}</p>
+                           </div>
                         </div>
-                    </div>
+                    )
+                })}
+                </div>
+                <div className='col2'>
+                </div>
+                <div className='col3'>
+                <h3 className='mb-2'><span>popular</span> Posted</h3>
+               {firstfourItems.map((item)=>{
+                    return(
+                        <div className='d-flex flex-row gap-3 mt-5 p-16'>
+                           <div className='cart-image'>
+                           <img src={item.image} alt='travel' className='image-two' />
+                           </div>
+                           <div className='card-text'>
+                           <span className='category'>{item.category}</span>
+                           <h5 className='title'>{item.title}</h5>
+                           <ul className='list-unstyled d-flex flex-row gap-3'>
+                            <li><img src={item.authorPicture} alt='authors phot' className='iamge-round'/></li>
+                            <li className='text-nowrap'>{item.date}</li>
+                            <li className='text-nowrap'>{item.time}</li>
+                           </ul>
+                           </div>
+                        </div>
+                    )
+                })} 
+
                 </div>
             </div>
 
-        <div className='main-container2'>
-        <div className='container-card2'>
-            <div className='col01'>
-                <h3><span>Recently</span> Posted</h3>
-                <div className='wrapper'>
-                        {Data.map((item, key)=>{
-                            return(
-                                <div key={key}  className='sub-col1'>
-                                    <a href='/javascri'>
-                                    <img src={item.image} alt='photo' />    
-                                    <div className='text-sec'>
-                                    <div>
-                                        <span  className='category'>{item.category}</span>
-                                    </div>
-                                    <div>
-                                        <h1>{item.title}</h1>
-                                    </div>
-                                    <div className='info'>
-                                    <span><img className='author-pic' src={item.authorPicture} alt='photo' /></span>
-                                    <span>{item.authorName}</span>
-                                    <span>{item.date}</span>
-                                    <span>{item.time}</span>
-                                    </div>
-                                    <div><p className='cat'>{item.desc}</p></div>    
-                                    </div>
-                                    </a>
-                                
-                                </div>
-                            )
-                        })}
-                        </div>
-
-                       <div className='container number-slider'>
-                      <button><a href='/prev'>Prev</a></button>
-                      <button><a href='/'>1</a></button>
-                      <button><a href='/'>2</a></button>
-                      <button><a href='/'>3</a></button>
-                      <button className='active'><a href='/next'>Next</a></button>
-                     </div>
-                        
-            </div>
-
-            <div className='col02'>
-                <div className='sub-col1'>
-                    <h3><span>top </span>authors</h3>
-                    {authorData.map((item, key)=>{
-                        return(
-                            <div key={key} className='sub-col01'>
-                                <div>
-                               <img src={item.image} alt='photo' />    
-                                </div>
-                                <div className='sub-col03'>
-                               <div>
-                                <p className='name'>{item.name}</p>
-                               </div>
-                               <div><p className='prof'>{item.profession}</p></div>
-                               <div className='sub-col02'>
-                                <img src={item.facebook} alt='photo' />
-                                <img src={item.instagram} alt='photo' />
-                                <img src={item.twiiter} alt='photo' />
-                               </div>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className='sub-col2'>
-                <span className='AD'>Ad</span>
-                    <div>want to travel sikkim by car?</div>
-                    <p>Did you come here for something in particular
-                     or just general Riker-bashing? And blowing into</p>
-                    <button className="btn-info"><a href='#btn'>Visit Us</a></button>
-                </div>
-                <div className='sub-col3'>
-                    <h3><span>Categories</span></h3>
-                    <div>
-                        <div className='flex'>
-                            <span><h6>lifestyle</h6></span>
-                            <span><h6>09</h6></span>
-                        </div>
-                        <hr/>
-                        <div className='flex'>
-                            <span><h6>Travel</h6></span>
-                            <span><h6>05</h6></span>
-                        </div>
-                        <hr/>
-                        <div className='flex'>
-                            <span><h6>Food</h6></span>
-                            <span><h6>09</h6></span>
-                        </div>
-                        <hr/>
-                        <div className='flex'>
-                            <span><h6>Healthcare</h6></span>
-                            <span><h6>09</h6></span>
-                        </div>
-                        <hr/>
-                        <div className='flex'>
-                            <span><h6>Technology</h6></span>
-                            <span><h6>09</h6></span>
-                        </div>
-                    </div>
-                </div>
-                <div className='sub-col4'>
-                    <h3><span>Today’s</span> update</h3>
-                    <div className='flex-item'>
-                        <div>
-                            <h2>14</h2>
-                            <p>New posts</p>
-                        </div>
-                        <div>
-                            <h2>480</h2>
-                            <p>Total Visistors</p>
-                        </div>
-                        <div>
-                            <h2>29</h2>
-                            <p>New Subscribers</p>
-                        </div>
-                        <div>
-                            <h2>138</h2>
-                            <p>Blog Read</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='sub-col5'>
-                    <h3><span>Instagram</span> posts</h3>
-                    <div className='flex'>
-                        <div className='flex-item'>
-                        <div className='flex-item'><img src='Images/insta3.jpeg'/></div>
-                        </div>
-                        <div className='flex-item'><img src='Images/insta.jpeg'/></div>
-                        <div className='flex-item'><img src='Images/insta1.jpeg'/></div>
-                        <div className='flex-item'><img src='Images/insta2.jpeg'/></div>
-                        <div className='flex-item'><img src='Images/insta3.jpeg'/></div>
-                        <div className='flex-item'><img src='Images/insta4.jpeg' /></div>
-                        <div className='flex-item'><img src='Images/insta1.jpeg'/></div>
-                        <div className='flex-item'><img src='Images/insta4.jpeg' /></div>
-                        <div className='flex-item'><img src='Images/insta1.jpeg' /></div>
-                    </div>
-                </div>
-                <div className='sub-col6'>
-                <h3><span>search</span> with tags</h3>
-                <div className='flex'>
-                    <div><a href='/travel'>Travel</a></div>
-                    <div><a href='/travel'>Lifestyle</a></div>
-                    <div><a href='/travel'>Fashion</a></div>
-                    <div className='active'><a href='/travel'>Technology</a></div>
-                    <div><a href='/travel'>Business</a></div>
-                    <div><a href='/travel'>Design</a></div>
-                    <div><a href='/travel'>Health</a></div>
-                    <div><a href='/travel'>Food</a></div>
-                    <div><a href='/travel'>Art</a></div>
-                </div>
-                </div>
-            </div>
         </div>
-        </div>
-        </div>
-    );
-}
+    )}
 
-export default Home;
+    export default Home;
